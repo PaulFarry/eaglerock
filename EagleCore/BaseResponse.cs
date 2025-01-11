04bf7f677f9caf259042145930a36fd0f85b2240
Paul Farry <paulfarry@gmail.com>
2025-01-11 20:16:06 +1000

    Add Static for Successful

--- EagleCore/BaseResponse.cs
+++ EagleCore/BaseResponse.cs
@@ -2,6 +2,8 @@
 {
 	public class BaseResponse
 	{
+		public static BaseResponse Successful = new() { Success = true };
+		public BaseResponse() => Message = string.Empty;
 		public string Message { get; set; }
 		public bool Success { get; set; }
 	}
